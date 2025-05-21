5793ad45e00899a3652d2fa448438af053f57800
Jean-Marc <dunker35@hotmail.fr>
2025-05-21 23:25:05 +0200

    scroll bugs fix

--- components/about/hero.tsx
+++ components/about/hero.tsx
@@ -1,48 +1,29 @@
 import { motion, useScroll, useTransform } from "framer-motion";
-import Image from "next/image";
-import heroImage from "@/public/assets/images/8E2A8316.webp";
 import { useRef } from "react";
 import { delayedFade } from "@/helpers/transitions";
 
 export const HeroSection = () => {
-  const { scrollYProgress } = useScroll({
-    offset: ["start start", "end start"],
-  });
-
-  const y = useTransform(scrollYProgress, [0, 1], ["0vh", "150vh"]);
-
-  // Animation sur l'axe X (horizontal)
-  const x = useTransform(scrollYProgress, [0, 1], ["0%", "-300%"]);
-
   return (
     <section className="relative h-[120vh] w-full overflow-hidden">
-      {/* Animation fade-in pour l'image */}
-      <motion.div
-        style={{ y }}
-        className="h-full relative will-change-transform"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1 }}
-      >
-        <Image
-          src={heroImage}
-          alt="hero image"
-          layout="fill"
-          objectFit="cover"
-          style={{ objectPosition: "30% 20%" }}
-          priority
-          placeholder="blur"
+      {/* Motion background */}
+      <div className="absolute inset-0">
+        <div
+          className="w-full h-full bg-cover bg-center"
+          style={{
+            backgroundImage: 'url("/assets/images/8E2A8316.webp")',
+            backgroundPosition: "30% 20%",
+          }}
         />
-      </motion.div>
+      </div>
 
-      <div className="absolute inset-0 bg-opacity-70 bg-gradient-to-t from-black via-black/50 to-transparent  flex flex-col justify-center items-start px-8 md:px-18 lg:px-24">
-        {/* Animation du texte avec un délai */}
+      {/* Overlay + Texte */}
+      <div className="absolute inset-0 bg-opacity-70 bg-gradient-to-t from-black via-black/50 to-transparent flex flex-col justify-center items-start px-8 md:px-18 lg:px-24">
         <motion.h1
           className="text-white text-7xl md:text-[10rem] lg:text-[11rem] leading-[0.9] tracking-tight uppercase font-neueBold"
           variants={delayedFade}
           initial="initial"
           animate="enter"
-          transition={{ delay: 1 }} // Le texte apparaîtra après l'image (1 seconde)
+          transition={{ delay: 1 }}
         >
           About
           <br />
--- components/about/section.tsx
+++ components/about/section.tsx
@@ -4,24 +4,20 @@ import Image from "next/image";
 import background from "@/public/assets/images/8E2A8178.webp";
 
 export default function Section() {
-  const { scrollYProgress } = useScroll({
-    offset: ["start end", "end start"],
-  });
-
-  const y = useTransform(scrollYProgress, [0, 1], ["-10vh", "10vh"]);
-
   return (
     <div
       className="relative flex items-center justify-center h-screen overflow-hidden"
       style={{ clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)" }}
     >
       <div className="fixed top-[-10vh] left-0 h-[120vh] w-full">
-        <motion.div style={{ y }} className="relative w-full h-full">
+        <motion.div className="relative w-full h-full">
           <Image
             src={background}
             fill
             alt="image"
             style={{ objectFit: "cover" }}
+            priority
+            placeholder="blur"
           />
         </motion.div>
       </div>
